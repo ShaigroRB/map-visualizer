f149b8930b72f12a03b2fa21e224fe4c5c56ef05
ShaigroRB <robin.boucher@epita.fr>
2020-10-21 22:06:04 +0200

    add BlockObject for blocks & add its children objects

---
+++ js/lib/lib-map/assets/blocks.js
@@ -0,0 +1,36 @@
+class BlockObject extends AssetObject {
+    constructor(id, name, x, y, objIsTile, objIndexId, depth, logicId) {
+        super(id, name, x, y, objIsTile, objIndexId, depth, logicId);
+        this.fillColor = "black";
+    }
+}
+
+class Block2x2Object extends BlockObject {
+    constructor(id, name, x, y, objIsTile, objIndexId, depth, logicId) {
+        super(id, name, x, y, objIsTile, objIndexId, depth, logicId);
+        this.widthCoeff = 2;
+        this.heightCoeff = 2;
+    }
+}
+
+class Block4x1Object extends BlockObject {
+    constructor(id, name, x, y, objIsTile, objIndexId, depth, logicId) {
+        super(id, name, x, y, objIsTile, objIndexId, depth, logicId);
+        this.widthCoeff = 4;
+    }
+}
+
+class Block1x4Object extends BlockObject {
+    constructor(id, name, x, y, objIsTile, objIndexId, depth, logicId) {
+        super(id, name, x, y, objIsTile, objIndexId, depth, logicId);
+        this.heightCoeff = 4;
+    }
+}
+
+class WallObject extends BlockObject {
+    constructor(id, name, x, y, objIsTile, objIndexId, depth, logicId, objWallWidth, objWallHeight, gridSize) {
+        super(id, name, x, y, objIsTile, objIndexId, depth, logicId);
+        this.widthCoeff = Math.floor(parseInt(objWallWidth) / gridSize);
+        this.heightCoeff = Math.floor(parseInt(objWallHeight) / gridSize);
+    }
+}
